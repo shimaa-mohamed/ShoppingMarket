c6dfbb48bbf77b6d2c3482ac27ee8de3d305fa16
Shimaa Mohamed <shimaamohamed18286@gmail.com>
2021-10-26 18:19:57 +0200

    create initial folder structure for the project

---
+++ src/components/CartPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cartpage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Cartpage;
---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Productitem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Productitem;
